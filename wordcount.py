26c18e95e2fd1ac0c15f3f0aa5ff57e42e5778d4
Jim Harter <james.r.harter.ii@gmail.com>
2019-10-02 19:47:17 -0500

    revised word counter and sorting functions

--- wordcount.py
+++ wordcount.py
@@ -1,7 +1,22 @@
+#Takes a list of strings, outputs a list of dictionaries in the following form:
+#   [{"word": <String>, "count": <Number>}]
 def processWordList(wordList):
-    uniqueWords = {word:0 for word in set(wordList)}
+
+    uniqueWordsDict = {word:0 for word in set(wordList)}
 
     for word in wordList:
-        uniqueWords[word] = uniqueWords + 1
+        uniqueWordsDict[word] = uniqueWordsDict[word] + 1
 
-    return uniqueWords
+    uniqueWordsList = []
+    for word, count in uniqueWordsDict.items():
+        uniqueWordsList.append({"word": word, "count": count})
+    
+    return uniqueWordsList
+
+#To be passed to `list.sort()` as key
+def sortWordsByCount(wordDict):
+    return wordDict["count"]
+
+#To be passed to `list.sort()` as key
+def sortWordsByAlpha(wordDict):
+    return wordDict["word"]
