3b272eefb9004ba7668c285ef152ddce5a299057
traipsinglutrinae <31224956+traipsinglutrinae@users.noreply.github.com>
2019-10-03 23:09:03 -0500

    Pretty much done?
    Needs abstract/intro comment and thats about it. Graph is pretty and polished up. many improvements from last version. If i fix the "not responding" thing with the pygame window then i cant wait for enter to exit. I dont really think its a problem.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import re
 import sys
 import pygame
-import time
+import collections
 
 
 # Function: parseFile
@@ -60,6 +60,7 @@ def processCleanWordList(wordList):
 # Function: sortWordsByCount
 # Use: To be passed to `list.sort()` as key
 def sortWordsByCount(wordDict):
+    wordDict = collections.OrderedDict(sorted(wordDict.items(), key=lambda x: x[1], reverse=True))
     return wordDict
 
 
@@ -69,6 +70,7 @@ def sortWordsByAlpha(wordDict):
     return wordDict["word"]
 
 
+# Returns length of the longest word used to set min col width
 def getLongestWord(wordList):
     maxWordLen = 1
     for word in wordList:
@@ -76,24 +78,33 @@ def getLongestWord(wordList):
             maxWordLen = len(word)
     return maxWordLen
 
-#Graphical output
+
+# Returns maximum number of times any one word occurs
+def getMostWord(wordDict):
+    maxWord = 1
+    for word in wordDict:
+        if wordDict[word] > maxWord:
+            maxWord = wordDict[word]
+    return maxWord
+
+
+# Graphical output
 def drawGraph(words, dicts):
 
     clock = pygame.time.Clock()  # manages screen update and animation speed
-    grey = (100, 100, 100)  # grid color
+    grey = (75, 75, 75)  # grid color
     black = (0, 0, 0)
     orange = (255, 140, 0)
-    purple = (180, 0, 255)
-    yellow = (255, 255, 0)
+    purple = (100, 0, 255)
 
-    barWidth = 7 * getLongestWord(words)  # size of each cell
+    barWidth = 6 * getLongestWord(words)  # dynamic col width to fit longest word
     barHeight = 15
 
     pygame.init()  # start pygame for generating visual output
     pygame.display.set_caption("Dictionary Graph")  # set window title
     font = pygame.font.Font(pygame.font.get_default_font(), 12)
-    windowX =22 * barWidth
-    windowY = len(dicts) * barHeight
+    windowX = 22 * barWidth
+    windowY = 22 * barHeight
     gameDisplay = pygame.display.set_mode((windowX, windowY))
     gameDisplay.fill(black)  # set background color
 
@@ -110,25 +121,36 @@ def drawGraph(words, dicts):
                 itr = itr + 1
                 posX = barWidth * itr  # get x position
                 posYtop = dicts[word] * barHeight
-                posYbot = barHeight
 
-                for r in range(len(dicts) + 2):  # draw graph lines
-                    pygame.draw.line(gameDisplay, grey, (r * barWidth, 0), (r * barWidth, len(dicts) * barWidth), 1)
-                pygame.draw.rect(gameDisplay, purple, (posX, windowY - 3*posYbot, barWidth, -posYtop))  # draw word bar
-                pygame.draw.line(gameDisplay, orange, (posX, windowY - 3*posYbot), (posX + barWidth, windowY - 3*posYbot), 2)  # draw y axis
-                pygame.draw.line(gameDisplay, orange, (barWidth, windowY - 3*posYbot), (barWidth, 0), 3)  # draw x axis
-                pygame.draw.line(gameDisplay, yellow, (barWidth, windowY - 3*posYbot - posYtop - 1), (posX + barWidth, windowY - 3*posYbot - posYtop - 1), 1)  # draw top of bar indicator line
+                # Draw each shaded word col
+                pygame.draw.rect(gameDisplay, purple, (posX, windowY - 3 * barHeight, barWidth, -posYtop))
 
+                # Draw each word label
                 wordLabel = font.render(word, False, orange)
-                countlabel = font.render(str(itr), False, orange)
-                gameDisplay.blit(wordLabel, (posX + 2, windowY - 2*posYbot))  # draw x axis labels
-                gameDisplay.blit(countlabel, (barWidth-20, windowY - (itr * barHeight) - 3 * barHeight))  # draw y axis labels
+                gameDisplay.blit(wordLabel, (posX + 2, windowY - 2*barHeight))  # draw x axis labels
 
-                pygame.event.pump()
-                pygame.display.update()  # update screen
-                clock.tick(30)  # variable display speed control based on size of the grid.
-    input()  # wait to kill graph
-    pygame.quit()
+            # Draw grid
+            for r in range(22):  # draw vertical graph lines
+                pygame.draw.line(gameDisplay, grey, (r * barWidth, 0), (r * barWidth, windowY - 3 * barHeight), 1)
+            for r in range(20):  # draw horizontal graph lines
+                pygame.draw.line(gameDisplay, grey, (barWidth, windowY - r * barHeight - 3 * barHeight), (21 * barWidth, windowY - r * barHeight - 3 * barHeight), 1)
+
+            # Draw x & y axis
+            pygame.draw.line(gameDisplay, orange, (barWidth, windowY - 3 * barHeight), (21 * barWidth, windowY - 3 * barHeight), 2)  # draw x axis
+            pygame.draw.line(gameDisplay, orange, (barWidth, windowY - 3 * barHeight), (barWidth, 0), 3)  # draw y axis
+
+            # Draw y axis labels
+            for i in range(getMostWord(dicts)):
+                countlabel = font.render(str(i + 1), False, orange)
+                gameDisplay.blit(countlabel, (barWidth-20, windowY - (( i + 1) * barHeight) - 3 * barHeight))
+
+                # update screen
+            pygame.event.pump()
+            pygame.display.update()
+            clock.tick(30)
+            input()  # wait to kill graph
+            pygame.quit()
+
 
 inFile = "test1.txt"
 
@@ -140,11 +162,11 @@ print(len(words))
 wordDicts = processWordList(words)
 print(wordDicts)
 
+print("\nPress enter to exit graphical output")
 cleanDict = processCleanWordList(words)  # remove display strings
-cleanDict = sortWordsByCount(cleanDict)  # sort to highest 20 get graphed
+cleanDict = sortWordsByCount(cleanDict)  # sort so highest 20 get graphed
 drawGraph(words, cleanDict)  # graph it
 
 #I noticed that it seems to have some issue with EOF. if there is no return line after the last word it fails to count
 # the last word. probably due to how that extra space was being handled.
 # Graph scales really weird with truly gigantic words
-# count sort needs finished. until then it just cuts off whatever happens to be past word 20 in the list
